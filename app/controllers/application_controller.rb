70bdf4902cb7d388a70d9febfa5d640dcaa84d0e
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2019-10-05 02:06:33 -0400

    first build

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  include ::ActionController::Cookies
+  # protect_from_forgery unless: -> { request.format.json? }
+# before_action :verify_authenticity_token
+
+
+  # before_action :authorized
+  # helper_method :current_user
+  #
+  def current_user
+    User.find_by({id: session[:user_id]})
+    # User.find_by(session[:user_id])
+
+  end
+  #
+  def logged_in?
+    !!current_user
+  end
+
+  # def authorized
+  #   redirect_to login_path unless logged_in?
+  # end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+
+
+    def create
+      @user = User.find_by(name: params[:name])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        render json: @user
+      else
+        render json: {
+          error: 'invalid credentials'
+        }
+      end
+    end
+
+    def get_current_user
+      if logged_in?
+        render json: current_user
+      else
+        render json: {
+          error: 'no one logged in'
+        }
+      end
+    end
+
+    def destroy
+      session.clear
+        render json: {
+        notice: "successfully logged out"
+      }, status: :ok
+    end
+
+
+
+end
