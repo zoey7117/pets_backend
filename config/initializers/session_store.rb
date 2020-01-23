f48d9c216da28b62386b313412ac23da999dc4e0
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-01-23 14:59:33 -0500

    modified session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
-if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_adopt-a-pet', domain: 'your-frontend-domain'
-else
-  Rails.application.config.session_store :cookie_store, key: '_adopt-a-pet'
-end
+# if Rails.env === 'production'
+#   Rails.application.config.session_store :cookie_store, key: '_adopt-a-pet', domain: 'your-frontend-domain'
+# else
+#   Rails.application.config.session_store :cookie_store, key: '_adopt-a-pet'
+# end
+
+PetsBackend::Application.config.session_store :cookie_store, key: 'adopt_a_pet_session'
