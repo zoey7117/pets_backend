4dabe63b3ef2f6bdfdb4c4db61065e3c949b11c2
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-01-25 19:13:59 -0500

    corrected http cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'https://fast-waters-11750.herokuapp.com | *'
-    origins 'https://pets-backend-api.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002','http://pets-backend-api.herokuapp.com'
+    origins 'https://fast-waters-11750.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002','http://fast-waters-11750.herokuapp.com'
 
     resource '*',
       headers: :any,
