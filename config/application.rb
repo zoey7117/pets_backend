ef19a273720b89377390428f9204f9baf3fb580a
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-01-25 19:11:55 -0500

    cookie

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module PetsBackend
 
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, key: '_cookie_name'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
